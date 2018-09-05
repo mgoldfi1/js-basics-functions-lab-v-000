7629fba56b85d13cb3861576db091d7e6360dc2b
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-05 19:09:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+  function distanceFromHqInBlocks(street) {
+    return Math.abs(street - 42)
+  }
+
+  function distanceFromHqInFeet(street) {
+    return Math.abs(street - 42)*264
+
+  }
+
+  function distanceTravelledInFeet(x,y) {
+    return Math.abs(x-y)*264
+  }
+
+  function calculatesFarePrice(start,end) {
+    if (distanceTravelledInFeet(start,end) <= 400) {
+      return 0
+    } else if  (distanceTravelledInFeet(start,end) >= 2500) {
+      return 'cannot travel that far'
+    }
+    else if (distanceTravelledInFeet(start,end) >= 2000) {
+      return 25}
+      else{
+    return (distanceTravelledInFeet(start,end) - 400)*.02}
+  }
